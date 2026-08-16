4f8b54d8ee00d8674ebf0a4b02b13a429c54f18e
00nikita <nikhithadubbaka2003@gmail.com>
2026-08-16 22:15:28 +0530

    redis-cli, printing request lines

--- server.py
+++ server.py
@@ -4,27 +4,29 @@ import json
 with open("config.json") as f:
     config = json.load(f)
 
-def handle_client(client_connection, client_address):
-    buffer = b""
+def handle_client(client_connection, client_address, buffer):
     request = []
-    while b"\r\n" not in buffer:
-        buffer += client_connection.recv(1024)
-    tot_words, remaining_part = buffer.split(b"\r\n", 1)
-    count = 2*int(tot_words.decode().split("$",1)[1])
-    flag = 0
-    while count > 0:
-        if b"\r\n" in remaining_part:
-            if flag == 0:
-                flag = 1 
-                continue 
-            request_line = remaining_part.split(b"\r\n", 1)[0].decode()
-            request.append(request_line)
-            remaining_part = remaining_part.split(b"\r\n", 1)[1]
-            count -= 1
-        else:
-            remaining_part += client_connection.recv(1024)
-
-    return request
+    while True:
+        while b"\r\n" not in buffer:
+            buffer += client_connection.recv(1024)
+        tot_words, remaining_part = buffer.split(b"\r\n", 1)
+        count = 2*int(tot_words.decode().split("*",1)[1])
+        flag = 0
+        while count > 0:
+            if b"\r\n" in remaining_part:
+                if flag == 0:
+                    flag = 1 
+                    continue 
+                request_line = remaining_part.split(b"\r\n", 1)[0].decode()
+                request.append(request_line)
+                remaining_part = remaining_part.split(b"\r\n", 1)[1]
+                flag = 0
+                count -= 1
+            else:
+                remaining_part += client_connection.recv(1024)
+        buffer = remaining_part
+        # RESP_parser(request, client_connection, client_address)
+        print(request)
 
 #creating socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,4 +40,4 @@ socket.listen()
 
 client_connection, client_address = socket.accept()
 
-handle_client(client_connection, client_address)
+handle_client(client_connection, client_address, b"")
