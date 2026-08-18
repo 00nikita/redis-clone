318a40ea9e00818d88a74278dd799818fe131226
00nikita <nikhithadubbaka2003@gmail.com>
2026-08-18 21:52:08 +0530

    SET, GET, DEL, EXISTS basic implementation

--- server.py
+++ server.py
@@ -1,13 +1,41 @@
 import socket
 import json 
 
+database = {}
+
 with open("config.json") as f:
     config = json.load(f)
 
 def execute_command(request):
     if request == ["PING"]:
         return b"+PONG\r\n"
-    return b"+OK\r\n"
+    elif request[0] == "SET":
+        key = request[1]
+        value = request[2]
+        database[key] = value
+        return b"+OK\r\n"
+    elif request[0] == "GET":
+        key = request[1]
+        if key in database:
+            value = database[key]
+            return f"${len(value)}\r\n{value}\r\n".encode()
+        else:
+            return b"$-1\r\n"
+    elif request[0] == "DEL":
+        key = request[1]
+        if key in database:
+            del database[key]
+            return b":1\r\n"
+        return b":0\r\n"
+    elif request[0] == "EXISTS":
+        key = request[1]
+        if key in database:
+            return b":1\r\n"
+        return b":0\r\n"
+    else:
+        return b"-ERROR: Unknown command\r\n"
+            
+        
 
 def handle_client(client_connection, client_address, buffer):
     while True:
