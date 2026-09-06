a78e5f21fdfccb3b7b16c3d67e4f854de5dc20ed
00nikita <nikhithadubbaka2003@gmail.com>
2026-09-06 22:03:27 +0530

    LIST data structure

--- commands.py
+++ commands.py
@@ -71,5 +71,112 @@ def execute_command(request, persist=False):
                 return f":{ttl}\r\n".encode()
             return b":-1\r\n"
         return b":-2\r\n"
+    elif request[0] == "LPUSH":
+        key = request[1]
+        value = request[2]
+        if key not in database:
+            database[key] = []
+        database[key].insert(0, value)
+        if persist:
+            with open("appendonly.aof", "a") as f:
+                f.write(" ".join(request) + "\n")
+        return b":1\r\n"
+    elif request[0] == "RPUSH":
+        key = request[1]
+        value = request[2]
+        if key not in database:
+            database[key] = []
+        database[key].append(value)
+        if persist:
+            with open("appendonly.aof", "a") as f:
+                f.write(" ".join(request) + "\n")
+        return b":1\r\n"
+    elif request[0] == "LRANGE":
+        key = request[1]
+        start = int(request[2])
+        end = int(request[3])
+        if key in database:
+            if isinstance(database[key], list):
+                if end == -1:
+                    values = database[key][start:]
+                else:
+                    values = database[key][start:end+1]
+                response = f"*{len(values)}\r\n"
+                for value in values:
+                    response += f"${len(value)}\r\n{value}\r\n"
+                return response.encode()
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
+    elif request[0] == "LLEN":
+        key = request[1]
+        if key in database:
+            if isinstance(database[key], list):
+                return f":{len(database[key])}\r\n".encode()
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
+    elif request[0] == "LINDEX":
+        key = request[1]
+        if key in database:
+            if isinstance(database[key], list):
+                value = database[key][int(request[2])]
+                return f"${len(value)}\r\n{value}\r\n".encode()
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
+    elif request[0] == "LSET":
+        key = request[1]
+        index = int(request[2])
+        value = request[3]
+        if key in database:
+            if isinstance(database[key], list):
+                if 0 <= index < len(database[key]):
+                    database[key][index] = value
+                    if persist:
+                        with open("appendonly.aof", "a") as f:
+                            f.write(" ".join(request) + "\n")
+                    return b"+OK\r\n"
+                else:
+                    return b"-ERROR: Index out of range\r\n"
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
+
+    
+    elif request[0] == "LPOP":
+        key = request[1]
+        if key in database:
+            if isinstance(database[key], list):
+                if database[key]:
+                    value = database[key].pop(0)
+                    if persist:
+                        with open("appendonly.aof", "a") as f:
+                            f.write("LPOP {} {}\n".format(key, value))
+                    return f"${len(value)}\r\n{value}\r\n".encode()
+                return b"$-1\r\n"
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
+    elif request[0] == "RPOP":
+        key = request[1]
+        if key in database:
+            if isinstance(database[key], list):
+                if database[key]:
+                    value = database[key].pop()
+                    if persist:
+                        with open("appendonly.aof", "a") as f:
+                            f.write("RPOP {} {}\n".format(key, value))
+                    return f"${len(value)}\r\n{value}\r\n".encode()
+                return b"$-1\r\n"
+            else:
+                return b"-ERROR: Key is not a list\r\n"
+        else:
+            return b"*0\r\n"
     else:
         return b"-ERROR: Unknown command\r\n"
