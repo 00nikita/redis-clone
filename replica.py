5ea4ebc52d3e4ea01d2280e5637e07aed1b02a24
00nikita <nikhithadubbaka2003@gmail.com>
2026-09-12 19:53:15 +0530

    parsing commands received by replica server

--- replica.py
+++ replica.py
@@ -1,5 +1,7 @@
 import socket
 import time
+from commands import execute_command
+import database
 
 PRIMARY_HOST = "127.0.0.1"
 PRIMARY_PORT = 6379
@@ -10,7 +12,6 @@ replica_socket = socket.socket(
 
 replica_socket.connect((PRIMARY_HOST, PRIMARY_PORT))
 
-replica_socket.connect((PRIMARY_HOST, PRIMARY_PORT))
 
 # Send simplified handshake
 handshake = (
@@ -28,9 +29,24 @@ print("Primary:", response.decode())
 
 while True:
     data = replica_socket.recv(4096)
-
     if not data:
         print("Primary disconnected")
         break
+    request = []
+    tot_words, remaining_part = data.split(b"\r\n", 1)
+    count = int(tot_words.decode().split("*",1)[1])
+    flag = 0
+    complete = True
+    while count > 0:
+        if b"\r\n" in remaining_part:
+            if flag == 0:
+                flag = 1
+                remaining_part = remaining_part.split(b"\r\n", 1)[1]
+                continue
+            request_line = remaining_part.split(b"\r\n", 1)[0].decode()
+            request.append(request_line)
+            remaining_part = remaining_part.split(b"\r\n", 1)[1]
+            flag = 0
+            count -= 1
+    execute_command(request, persist=False, client_connection=replica_socket, executing=False, from_replica=True)
 
-    print("Received from primary:", data)
